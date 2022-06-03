6ca07525c817996c89dc7e82c844732e3d4f13ab
Jason Mandozzi <jason.mandozzi@gmail.com>
2022-06-02 22:18:42 -0400

    Initial Commit

---
+++ Shwordle/ShwordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShwordleApp.swift
+//  Shwordle
+//
+//  Created by Jason Mandozzi on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ShwordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
